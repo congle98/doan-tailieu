b7fc62d717f629271bf4d50b38d868a511dd76c2
Le Thanh Cong (PM) <conglt@nms.vn>
2021-10-21 17:54:40 +0700

    ole

---
+++ db/doan_shopping_city.sql
@@ -0,0 +1,6 @@
+INSERT INTO `doan-shopping`.city (id, name) VALUES (1, 'Hà Nội');
+INSERT INTO `doan-shopping`.city (id, name) VALUES (2, 'Hải Phòng');
+INSERT INTO `doan-shopping`.city (id, name) VALUES (3, 'Quảng Ninh');
+INSERT INTO `doan-shopping`.city (id, name) VALUES (4, 'Phú Thọ');
+INSERT INTO `doan-shopping`.city (id, name) VALUES (5, 'TP. Hồ Chí Minh');
+INSERT INTO `doan-shopping`.city (id, name) VALUES (6, 'Nha Trang');
---
+++ db/doan_shopping_role.sql
@@ -0,0 +1,2 @@
+INSERT INTO `doan-shopping`.role (id, name) VALUES (1, 'USER');
+INSERT INTO `doan-shopping`.role (id, name) VALUES (2, 'ADMIN');
